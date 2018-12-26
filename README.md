ac9518af6c36fa2eb6a1a900917f9a1fdac122f7
zqwei <zqwei@iflytek.com>
2018-12-26 15:41:35 +0800

    add zipFolder

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ some simple python script used in my work
 
 - emptyMail.py clear email inbox
 - picTools.py img reszie ,png -> ico
+- zipFolder.py compress a folder to zip file
---
+++ zipFolder.py
@@ -0,0 +1,20 @@
+#coding:utf-8
+import zipfile
+import argparse
+import os
+
+def zipFolder(baseDir,zip_name):
+  z = zipfile.ZipFile(zip_name,'w',zipfile.ZIP_DEFLATED) #参数一：文件夹名
+  for dirpath, dirnames, filenames in os.walk(baseDir):
+    fpath = dirpath.replace(baseDir,'') #这一句很重要，不replace的话，就从根目录开始复制
+    for filename in filenames:
+      z.write(os.path.join(dirpath, filename),os.path.join(fpath, filename))
+      print('压缩成功')
+  z.close()
+
+if __name__ =="__main__":
+  parser = argparse.ArgumentParser(description="python image tools")
+  parser.add_argument("src")
+  parser.add_argument("dst")
+  args = parser.parse_args()
+  zipFolder(args.src,args.dst)
